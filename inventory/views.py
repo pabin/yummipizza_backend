f35263990ce99b1db8a55cb7ebfe55d4b90a79bb
Pabin R Luitel <pabin2050@gmail.com>
2020-05-18 21:28:46 +0545

    Removed ast literal from Shopping cart retrieve or update API view

--- inventory/views.py
+++ inventory/views.py
@@ -56,8 +56,11 @@ class ShoppingCartRUAPIView(generics.RetrieveUpdateAPIView):
 
     def put(self, request, pk, format=None):
         cart = get_object_or_404(ShoppingCart, id=pk)
-        items = ast.literal_eval(request.data['items'])
-
+        # items = ast.literal_eval(request.data['items'])
+        items = request.data['items']
+        print("items", items)
+        print("items type", type(items))
+        
         for item in items:
             cart.items.add(item)
 
